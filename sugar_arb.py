42962a61e8576473f009377cb09a7d939ed521c6
dchin490 <52142224+dchin490@users.noreply.github.com>
2022-01-19 13:04:08 -0500

    Update sugar_arb.py

--- sugar_arb.py
+++ sugar_arb.py
@@ -1,13 +1,15 @@
-
 import streamlit as st
 import math
 
+#arb premium calc
 def sugar_arb(sb, w):
     prem = abs((sb*22.0462)-w)
     prem = round(prem, 5)
     return prem
 
-w = st.number_input("LDN PRICE", min_value=0.0, max_value=None, step = 0.1)
-sb = st.number_input("NY PRICE", min_value=0.00, max_value=None, step = 0.01)
-st.metric("ARB PREMIUM", sugar_arb(sb, w), delta=None, delta_color="normal")
+#assign user input prices to sugar legs 
+w = st.number_input("White Sugar (W:IFLX)", min_value=0.0, max_value=None, step = 0.1)
+sb = st.number_input("Sugar No. 11 (SB:IFUS)", min_value=0.00, max_value=None, step = 0.01)
 
+#display arb premium 
+st.metric("ARB Premium", sugar_arb(sb, w), delta=None, delta_color="normal")
