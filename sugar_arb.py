f6772a46040bdfaf48d25aef4600b588020dd079
dchin490 <52142224+dchin490@users.noreply.github.com>
2022-01-19 13:57:07 -0500

    Update sugar_arb.py

--- sugar_arb.py
+++ sugar_arb.py
@@ -12,8 +12,8 @@ def sugar_arb(sb, w):
     return prem
 
 #assign user input prices to sugar legs 
-w = st.number_input("White Sugar (London)", min_value=0.0, max_value=None, value = 'None', step = 0.1)
-sb = st.number_input("Sugar No. 11 (New York)", min_value=0.00, max_value=None, value = 'None', step = 0.01)
+w = st.number_input("White Sugar (London)", min_value=0.0, max_value=None, step = 0.1)
+sb = st.number_input("Sugar No. 11 (New York)", min_value=0.00, max_value=None, step = 0.01)
 
 #display arb premium 
 st.metric("Premium", sugar_arb(sb, w), delta=None, delta_color="normal")
