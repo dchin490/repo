a4d6323aa209c0edf721a4c08579f766b14dc077
dchin490 <52142224+dchin490@users.noreply.github.com>
2022-01-19 11:00:37 -0500

    Update sugar_arb.py

--- sugar_arb.py
+++ sugar_arb.py
@@ -7,7 +7,7 @@ def sugar_arb(sb, w):
     prem = round(prem, 5)
     return prem
 
-w = st.number_input("LDN PRICE", min_value=None max_value=None, step = 0.1)
+w = st.number_input("LDN PRICE", min_value=None, max_value=None, step = 0.1)
 sb = st.number_input("NY PRICE", min_value=0.00, max_value=None)
 st.metric("ARB PREMIUM", sugar_arb(sb, w), delta=None, delta_color="normal")
 
